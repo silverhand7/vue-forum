75275fa16fb0da8fe695379f7e92a46d9bd4307a
silverhand <refojunior5@gmail.com>
2022-10-14 23:58:10 +0800

    post list component

---
+++ src/components/PostList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post.id" class="post">
+            <div class="user-info">
+                <a href="#" class="user-name">{{ userById(post.userId).name }}</a>
+                <a href="#">
+                    <img :src="userById(post.userId).avatar" alt="" class="avatar-large">
+                </a>
+                <p class="desktop-only text-small">102 posts</p>
+            </div>
+            <div class="post-content">
+                <div>
+                    <p>{{ post.text }}</p>
+                </div>
+            </div>
+            <div class="post-date text-faded">
+                {{ post.publishedAt }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import sourceData from '@/data.json';
+
+export default {
+    props: {
+        posts: {
+            required: true,
+            type: Array
+        }
+    },
+    data() {
+        return {
+            users: sourceData.users,
+        }
+    },
+    methods: {
+        userById(userId) {
+            return this.users.find(u => u.id === userId);
+        }
+    }
+}
+
+</script>
