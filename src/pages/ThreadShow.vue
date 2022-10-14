1303b99beade5217eeea04eeba25f798af9ad05c
silverhand <refojunior5@gmail.com>
2022-10-15 07:32:39 +0800

    form thread

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -3,6 +3,19 @@
         <h1>{{ thread.title }}</h1>
 
         <post-list :posts="threadPosts" />
+
+        <div class="col-full">
+            <form @submit.prevent="addPost">
+                <div class="form-group">
+                    <textarea
+                        v-model="newPostText"
+                        name="" id="" cols="30" rows="10" class="form-input"></textarea>
+                </div>
+                <div class="form-acions">
+                    <button class="btn-blue">Submit post</button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -26,6 +39,7 @@ export default {
         return {
             threads: sourceData.threads,
             posts: sourceData.posts,
+            newPostText: '',
         }
     },
     computed: {
@@ -34,6 +48,22 @@ export default {
         },
         threadPosts() {
             return this.posts.filter(post => post.threadId === this.id);
+        }
+    },
+    methods: {
+        addPost() {
+            const postId = 'ggg' + Math.random();
+            const post = {
+                id: postId,
+                text: this.newPostText,
+                publishedAt: Math.floor(Date.now() / 1000),
+                threadId: this.id,
+                userId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1'
+            }
+            this.posts.push(post);
+            this.thread.posts.push(postId);
+
+            this.newPostText = '';
         }
     }
 }
