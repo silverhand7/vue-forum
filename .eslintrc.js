ce6cd16ffbde3a63481b246a36de41dfe65bf5fe
silverhand <refojunior5@gmail.com>
2022-10-14 14:27:34 +0800

    eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "node": true
+    root: true,
+
+    env: {
+        node: true
     },
-    "extends": [
+
+    extends: [
         "plugin:vue/vue3-essential",
         "eslint:recommended"
     ],
-    "parserOptions": {
-        "parser": "@babel/eslint-parser"
+
+    parserOptions: {
+      parser: '@babel/eslint-parser'
     },
-    "rules": {
-        "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn"
+
+    rules: {
+        'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
     }
 }
