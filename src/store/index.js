00f2e12eb84e55b4c9e210659c022311feeb6915
silverhand <refojunior5@gmail.com>
2022-10-15 13:09:19 +0800

    vuex update globally

--- src/store/index.js
+++ src/store/index.js
@@ -2,5 +2,23 @@ import { createStore } from 'vuex';
 import sourceData from '@/data.json';
 
 export default createStore({
-    state: sourceData
+    state: sourceData,
+    actions: {
+        createPost(context, post) {
+            post.id = 'ggg' + Math.random();
+            context.commit('setPost', { post });
+            context.commit('appendPostToThread', {
+                postId: post.id,
+                threadId: post.threadId
+            });
+        }
+    },
+    mutations: {
+        setPost(state, { post }) {
+            state.posts.push(post);
+        },
+        appendPostToThread(state, { postId, threadId}) {
+            state.threads.find(thread => thread.id === threadId).posts.push(postId);
+        }
+    }
 });
