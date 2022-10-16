da881b7471ca8283a598d8ba0cb260dd4f9e090e
silverhand <refojunior5@gmail.com>
2022-10-16 16:48:10 +0800

    initialize firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apiKey: "AIzaSyDzkbZX1yM-PFrvzDGbS04xPjfndi7t-50",
+    authDomain: "vue-forum-eb557.firebaseapp.com",
+    projectId: "vue-forum-eb557",
+    storageBucket: "vue-forum-eb557.appspot.com",
+    messagingSenderId: "951021206025",
+    appId: "1:951021206025:web:df8056fe722e4fcc9296d8"
+}
